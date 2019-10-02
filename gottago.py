22d9858c1f8e4bdcfd871d73802469a3cd833694
CC <chrispfchung@gmail.com>
2019-10-01 23:36:39 -0400

    created web scraping for y combinator with bs4

--- gottago.py
+++ gottago.py
@@ -1,1 +1,15 @@
 1. "I want to walk for 15 minutes." Creates a random route that takes you for a 15 minute walk.
+
+
+walking - https://www.google.com/maps/dir/Syosset+LIRR+Station,+Jackson+Ave,+Syosset,+NY/37+Victor+Ln,+Woodbury,+NY+11797/@40.8261912,-73.4960979,15z/
+
+biking - https://www.google.com/maps/dir/Syosset+LIRR+Station,+Jackson+Ave,+Syosset,+NY/37+Victor+Ln,+Woodbury,+NY+11797/@40.8261912,-73.4960979,15z/
+
+https://www.google.com/maps/dir/Syosset+LIRR+Station,+Jackson+Ave,+Syosset,+NY/37+Victor+Ln,+Woodbury,+NY+11797/@40.8241573,-73.4969698,15z/am=t/data=!4m15!4m14!1m5!1m1!1s0x89c28248240839f5:0x7faaf256b73aa7f5!2m2!1d-73.5005032!2d40.8248559!1m5!1m1!1s0x89c2827058423b37:0x68ca015623635110!2m2!1d-73.474046!2d40.8278697!3e2!4e1
+
+
+
+data=!4m15!4m14!1m5!1m1!1s0x89c28248240839f5:0x7faaf256b73aa7f5!2m2!1d-73.5005032!2d40.8248559!1m5!1m1!1s0x89c2827058423b37:0x68ca015623635110!2m2!1d-73.474046!2d40.8278697!3e2!4e1
+
+
+data=!3m1!4b1!4m15!4m14!1m5!1m1!1s0x89c28248240839f5:0x7faaf256b73aa7f5!2m2!1d-73.5005032!2d40.8248559!1m5!1m1!1s0x89c2827058423b37:0x68ca015623635110!2m2!1d-73.474046!2d40.8278697!3e1!4e1
